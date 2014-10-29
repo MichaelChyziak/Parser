9556567230200095e733e0c3875a15f4b2f12d4f
TheChyz <thechyz@hotmail.com>
2014-10-28 20:43:49 -0700

    Update assignment2.cpp

--- assignment2.cpp
+++ assignment2.cpp
@@ -7,16 +7,62 @@ using namespace std;
 
 //Removes all inline comments from the tokenList including the // marker
 //Returns the number of comments removed
-int removeInlineComments(TokenList &tokenList) { 
-	//TO DO
-	return 0;
+//tokenList must be existing object
+int removeInlineComments(TokenList &tokenList) {\
+	int count = 0; //counts the amount of inline comments removed
+	Token* temp;
+	temp = tokenList.getFirst();
+	while (temp) {
+		if (temp->getStringRep() == "//") {
+			Token* destroy1;
+			destroy1 = temp;
+			if (temp->getNext() != NULL) {
+				Token* destroy2;
+				destroy2 = temp->getNext();
+				temp = destroy2->getNext();
+				tokenList.deleteToken(destroy1);
+				tokenList.deleteToken(destroy2);
+			}
+			else {
+				delete temp;
+			}
+		}
+		else {
+			temp = temp->getNext();
+
+		}
+	}
+	return count;
 }
 
 //Remove all block comments from the tokenList including /* and */ markers
 //Returns the number of block comments removed (where each pair of /* and */ count as one comment)
 int removeBlockComments(TokenList &tokenList) { 
-	//TO DO
-	return 0;
+	int count = 0;
+	bool deleted = false;
+	Token* temp, *destroy;
+	temp = tokenList.getFirst();
+	while (temp) {
+		if (temp->getStringRep() == "/*") {
+			count++;
+			while (!deleted) {
+				destroy = temp;
+				temp = temp ->getNext();
+				tokenList.deleteToken(destroy);
+				if (temp->getStringRep() == "*/") {
+					destroy = temp;
+					temp = temp->getNext();
+					deleted = true; 
+					tokenList.deleteToken(destroy);
+				}
+			}
+		}
+		else {
+			int k = 1;
+			temp = temp->getNext();
+		}
+	}
+	return count;
 }
 
 //Example Test code for interacting with your Token, TokenList, and Tokenizer classes
@@ -37,14 +83,12 @@ int main() {
 		string lineA, lineB;
 
 		getline(sourceFile, lineA);
-
 		//while the current line ends with a line-continuation \ append the next line to the current line
-		while (lineA[lineA.length() - 1] == '\\') {
+		while (lineA.length() > 0 && lineA[lineA.length() - 1] == '\\') {
 			lineA.erase(lineA.length() - 1, 1);
 			getline(sourceFile, lineB);
 			lineA += lineB;
 		}
-
 		tokenizer.setString(&lineA);
 		while (!tokenizer.isComplete()) {
 			tokens.append(tokenizer.getNextToken());
@@ -65,8 +109,8 @@ int main() {
 
 	for (int i = 0; t; i++) {
 		cout << "token" << i << ": " << t->getStringRep() << endl;
-		t = t->getNext();
+		t = t->getNext(); 
 	}
-
 	return 0;
+
 }
